e81db86ab9095ae89dbdce966f06f65bbc659b96
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2025-10-30 12:06:21 +0000

    MongoDb connection

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 # from typing import Union
+from fastapi import FastAPI, Request
 
-from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+db = client["notesApp"]
+collection = db["notes"]
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    collection.insert_one({"title": "Hello Mongo", "content": "Created from FastAPI"})
+    notes = list(collection.find({},{"_id":0}))
+    return templates.TemplateResponse(
+     "index.html",{"request":request, "notes":notes}
+    )
 
 
 @app.get("/items/{item_id}")
